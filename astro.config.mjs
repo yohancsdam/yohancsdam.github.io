ee2fd85d05fa4f561a5995cc2f8190a0e6f1d5c8
sombrilla420 <zombyra7@gmail.com>
2025-03-11 13:04:42 +0100

    d

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import alpinejs from '@astrojs/alpinejs';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [alpinejs()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
